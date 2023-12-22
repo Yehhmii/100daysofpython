9bad2a997be7978f00c9a4c9427736fe69d7af1b
iamYehhmii <franciscoabhulimen@gmail.com>
2023-12-23 00:09:07 +0100

    "

--- PycharmProjects/Day26/NATO-alphabet/main.py
+++ PycharmProjects/Day26/NATO-alphabet/main.py
@@ -7,7 +7,15 @@ data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(data_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_code = input("Enter a Word? : ").upper()
 
-phonetic_list = [data_dict[letter] for letter in user_code]
-print(phonetic_list)
+def generate_phonetic():
+    user_code = input("Enter a Word? : ").upper()
+    try:
+        phonetic_list = [data_dict[letter] for letter in user_code]
+    except KeyError:
+        print("Sorry only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
+
+generate_phonetic()
