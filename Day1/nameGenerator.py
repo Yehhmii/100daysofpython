8ec002e3f2a93cee9bc7fe767f04c30c004e571a
iamYehhmii <franciscoabhulimen@gmail.com>
2024-01-06 10:44:55 +0100

    update for days 20s

--- Day1/nameGenerator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to Food Site Generator")
-
-favor_word = input("What is you favourite word? \n")
-best_food = input("What is your favourite food \n")
-
-name = favor_word + " " + best_food
-print("Your food name could be:", name)
